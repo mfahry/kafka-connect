1f44db08a63626afcde588ca6ace8d8495f15c29
Muhammad Fahry <muhammadfahry18@gmail.com>
2018-02-08 15:21:30 +0700

    test

--- cassandra/setup-server.sh
+++ cassandra/setup-server.sh
@@ -65,31 +65,4 @@ mv kafka-connect-cli-0.5-all.jar /opt/confluent/plugins/streamreactor/libs/
 
 mkdir /opt/confluent/etc/kafka-connect-cassandra
 touch /opt/confluent/etc/kafka-connect-cassandra/quickstart-cassandra.properties
-
-
-name=cassandra-sink-orders
-connector.class=com.datamountaineer.streamreactor.connect.cassandra.sink.CassandraSinkConnector
-tasks.max=1
-topics=orders-topic
-connect.cassandra.export.route.query=INSERT INTO orders SELECT * FROM orders-topic
-connect.cassandra.contact.points=localhost
-connect.cassandra.port=9042
-connect.cassandra.key.space=demo
-connect.cassandra.username=cassandra
-connect.cassandra.password=cassandra
-
-java -jar /opt/confluent/plugins/streamreactor/libs/kafka-connect-cli-0.5-all.jar create cassandra-sink-orders < /opt/confluent/etc/kafka-connect-cassandra/quickstart-cassandra.properties
-
-bin/kafka-avro-console-producer \
- --broker-list localhost:9092 --topic orders-topic \
- --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"id","type":"int"},{"name":"created","type":"string"},{"name":"product","type":"string"},{"name":"price","type":"double"}, {"name":"qty", "type":"int"}]}'
-
-
-{"id": 1, "created": "2016-05-06 13:53:00", "product": "OP-DAX-P-20150201-95.7", "price": 94.2, "qty":100}
-{"id": 2, "created": "2016-05-06 13:54:00", "product": "OP-DAX-C-20150201-100", "price": 99.5, "qty":100}
-{"id": 3, "created": "2016-05-06 13:55:00", "product": "FU-DATAMOUNTAINEER-20150201-100", "price": 10000, "qty":100}
-{"id": 4, "created": "2016-05-06 13:56:00", "product": "FU-KOSPI-C-20150201-100", "price": 150, "qty":100}
-
-
-use demo;
-SELECT * FROM orders;
+ 
